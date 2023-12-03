712f51fcd45f368e04db6bb81f717d337660ea4f
nayaksomkar <nayaksomkar+github.dev@outlook.in>
2023-12-04 00:35:19 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ COLOR_ALIVE_NEXT = (255, 255, 255)
 
 
 def update(screen, cells, size, with_progress = False):
-    updated_cells = np.empty((cells.shape[0], cells.shape[1]))
+    updated_cells = np.zeros((cells.shape[0], cells.shape[1]))
 
     for row, col in np.ndindex(cells.shape):
         alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
