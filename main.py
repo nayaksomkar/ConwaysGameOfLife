cc922bd51cb01612dae7da66b6344c1be6029636
nayaksomkar <nayaksomkar+github.dev@outlook.in>
2023-12-03 23:53:57 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import time
 import pygame
 import numpy as np
 
-COLOR_BG = ()
+COLOR_BG = (10, 10, 10)
+COLOR_GRID = (40, 40, 40)
+COLOR_DIE_NEXT = (170, 170, 170)
+COLOR_ALIVE_NEXT = (255, 255, 255)
