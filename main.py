2f000023d06ac4edb8ea9aaad6571bd0068d3beb
nayaksomkar <nayaksomkar+github.dev@outlook.in>
2024-01-30 20:13:07 +0530

    new

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ COLOR_DIE_NEXT = (170, 170, 170)
 COLOR_ALIVE_NEXT = (255, 255, 255)
 
 
-def update(screen, cells, size, with_progress = False):
+def update(screen, cells, size, with_progress=False):
     updated_cells = np.zeros((cells.shape[0], cells.shape[1]))
 
     for row, col in np.ndindex(cells.shape):
-        alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
+        alive = np.sum(cells[row - 1:row + 2, col - 1:col + 2]) - cells[row, col]
 
         if cells[row, col] == 0:
             color = COLOR_BG
@@ -37,7 +37,6 @@ def update(screen, cells, size, with_progress = False):
                 if with_progress:
                     color = COLOR_ALIVE_NEXT
 
-
         pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
 
     return updated_cells
@@ -47,7 +46,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
 
-    cells = np.zeros((60,80))
+    cells = np.zeros((60, 80))
     screen.fill(COLOR_GRID)
     update(screen, cells, 10)
 
@@ -61,20 +60,18 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-            
+
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     running = not running
                     update(screen, cells, 10)
                     pygame.display.update()
 
-                
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
                 cells[pos[1] // 10, pos[0] // 10] = 1
                 update(screen, cells, 10)
                 pygame.display.update()
-
 
         screen.fill(COLOR_GRID)
 
@@ -82,8 +79,8 @@ def main():
             cells = update(screen, cells, 10, with_progress=True)
             pygame.display.update()
 
-
         time.sleep(0.001)
+
 
 if __name__ == "__main__":
     main()
